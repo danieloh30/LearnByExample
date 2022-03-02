8215ca38f6429127fa6662400f88df6a329a1b3b
danieloh30 <doh@redhat.com>
2022-03-01 23:23:51 -0500

    Update examples

---
+++ todo-demo-app/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a StatefulSet', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a Headless Service', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Deploy a TODO Quarkus application', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Do a Sanity Test', false, 0, null);
