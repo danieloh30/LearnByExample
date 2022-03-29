792fafa1c05026d9452f20375631910186415e5d
danieloh30 <doh@redhat.com>
2022-03-28 22:16:43 -0400

    Add example #9

--- todo-demo-app/src/main/resources/import.sql
+++ todo-demo-app/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a StatefulSet', false, 0, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a Headless Service', false, 0, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Deploy a TODO Quarkus application', false, 0, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Do a Sanity Test', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a PersistentVolume', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Create a PersistentVolumeClaim', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Deploy a PostgreSQL with PVC', false, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Deploy a Quarkus TODO App', false, 0, null);
